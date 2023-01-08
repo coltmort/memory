6247dd0cf81d9e625db4d74fbe5823e686f05292
ColtMort <balding.fenders.0e@icloud.com>
2023-01-08 11:20:18 -0700

    added reset cards function to wrong answers. still needs work

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,13 @@ var scoreLeft = 0
 var scoreRight = 0
 let possiblePositions = [1, 2, 3, 4, 5, 6, 7 ,8 ,9 ,10, 11, 12, 13, 14, 15, 16]
 let cardSelector = ['.c1', '.c2', '.c3', '.c4', '.c5', '.c6', '.c7', '.c8', '.c9', '.c10', '.c11', '.c12', '.c13', '.c14', '.c15', '.c16']
+var turnCounter = 1
+var turnOne
+var turnTwo
+var side = 'left'
 
+var leftScore = document.querySelector('.leftScore')
+var rightScore = document.querySelector('.rightScore')
 
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
@@ -13,14 +19,7 @@ function shuffleArray(array) {
     }
 }
 
-
-
 var positionC16 = document.querySelector('.c16').dataset.position
-
-
-
-
-
 
 // cards randomly assigned position
     // order set to random number, that number removed from set
@@ -36,16 +35,59 @@ randomizePosition()
 // cards flip
 document.querySelector('.container').addEventListener('click', function(event){
     var element = event.target;
-if (element.matches('.card')){
-  if (element.getAttribute('data-flipped')==='hidden'){
-    var number = element.getAttribute('data-pair')
-    element.textContent = number
-    element.setAttribute('data-flipped', 'visible')
-  } else {
-    element.innerHTML = "🤪"
-    element.setAttribute('data-flipped', 'hidden')
-  }
-}})
+    var turnOneCard
+    var turnTwoCard
+
+    if (element.matches('.card')){
+        if (turnCounter === 1){
+            if (element.getAttribute('data-flipped')==='hidden'){
+                turnCounter++
+                turnOne = element.getAttribute('data-pair')
+                element.textContent = turnOne
+                element.setAttribute('data-flipped', 'visible')
+                turnOneCard = element.classList[1]
+                console.log(turnOneCard)
+            } else {
+                return
+            }
+        } else {
+            turnCounter = 1
+            turnTwo = element.getAttribute('data-pair')
+            element.textContent = turnTwo
+            element.setAttribute('data-flipped', 'visible') 
+            turnTwoCard = element.classList[1]  
+            console.log(turnTwoCard)
+            if (side === 'left'){
+                if (turnOne === turnTwo){
+                    scoreLeft++
+                    leftScore.innerHTML = scoreLeft
+                    element.classList.add('left')
+                } else {
+                    side = 'right'
+                    resetCards()
+                    console.log('reset left')
+                }
+            } else {
+                if (turnOne === turnTwo){
+                    scoreRight++
+                    rightScore.innerHTML = scoreRight
+                }else{
+                    side = 'left'
+                    resetCards()
+                    console.log('reset right')
+                }
+            }
+        }
+    }
+    
+})
+function resetCards(){
+    document.querySelectorAll('.card').forEach(e => {
+        e.setAttribute('data-flipped', 'hidden')
+        e.textContent = ''
+    })
+}
+
 
 // if cards = score and cards move to side
 // same players turn
